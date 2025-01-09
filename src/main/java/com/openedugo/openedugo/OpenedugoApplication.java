102eb200840eb86ea80fcdccb026269b8d6b3ba5
devfrikz <tmendoza9204@gmail.com>
2025-01-08 23:08:58 -0600

    nw: primer commit del sistema

---
+++ src/main/java/com/openedugo/openedugo/OpenedugoApplication.java
@@ -0,0 +1,13 @@
+package com.openedugo.openedugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenedugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenedugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openedugo/openedugo/OpenedugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openedugo.openedugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenedugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
