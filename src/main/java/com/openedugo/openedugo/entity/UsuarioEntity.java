77216590aa38381d0c83f3bc6511fffc5212b490
devfrikz <tmendoza9204@gmail.com>
2025-01-19 11:56:57 -0600

    feat: actualizacion

--- src/main/java/com/openedugo/openedugo/entity/UsuarioEntity.java
+++ src/main/java/com/openedugo/openedugo/entity/UsuarioEntity.java
@@ -4,10 +4,119 @@
  */
 package com.openedugo.openedugo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 /**
  *
  * @author devfrikz
  */
+@Entity
+@Table(name="usuarios")
 public class UsuarioEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
+    private Integer id;
+    
+    @Column(name = "username",nullable = false, unique = true,length = 50)
+    private String username;
+    
+    @Column(name= "password", nullable = false, length = 255)
+    private String password;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id",nullable = false)
+    private Rol role;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sucursal_id")
+    private Sucursal sucursal;
+    
+    @Column(name="nombre", length = 100)
+    private String nombre;
+    
+    @Column(name="apellido",length = 100)
+    private String apellido;
+    
+    @Column(name ="email",length = 100)
+    private String email;
+
+    
+    // Getter y Setters
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRole() {
+        return role;
+    }
+
+    public void setRole(Rol role) {
+        this.role = role;
+    }
+
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     
 }
