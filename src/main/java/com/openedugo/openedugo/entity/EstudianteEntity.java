8ab2c780f745b540a700d2e2f9631d68ec2bdc8b
Joao Pinell <joaopinell20@gmail.com>
2025-01-27 14:54:37 -0600

    update

--- src/main/java/com/openedugo/openedugo/entity/EstudianteEntity.java
+++ src/main/java/com/openedugo/openedugo/entity/EstudianteEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.Date;
+import java.sql.Date;
 
 @Entity
 @Table(name = "estudiantes")
