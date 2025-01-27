379dc822766bcef537d5dd846d205b42fa6b207c
Joao Pinell <joaopinell20@gmail.com>
2025-01-27 13:20:33 -0600

    Roles permisos entity

---
+++ src/main/java/com/openedugo/openedugo/entity/RolePermisoEntity.java
@@ -0,0 +1,47 @@
+package com.openedugo.openedugo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles_permisos")
+public class RolePermisoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Column(name = "role_id", nullable = false)
+    private Integer roleId;
+            
+    @Column(name = "permiso_id", nullable = false)
+    private Integer permisoId;
+
+    public RolePermisoEntity() {
+    }
+
+    public RolePermisoEntity(Integer roleId, Integer permisoId) {
+        this.roleId = roleId;
+        this.permisoId = permisoId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getPermisoId() {
+        return permisoId;
+    }
+
+    public void setPermisoId(Integer permisoId) {
+        this.permisoId = permisoId;
+    }
+    
+    
+}
